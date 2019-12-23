e31d381f66289104d75498f311314e99fc81c063
inf117226 <mateusz.ledzianowski@student.put.poznan.pl>
2019-12-23 17:14:08 +0100

    Configure MediatR.

--- CardCreator/App.xaml.cs
+++ CardCreator/App.xaml.cs
@@ -1,9 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CardCreator.Features.Cards;
+using CardCreator.Features.Drawing;
+using CardCreator.Features.Fonts;
+using CardCreator.Features.Images;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+using System.Reflection;
 using System.Windows;
 
 namespace CardCreator
@@ -13,9 +17,39 @@ namespace CardCreator
     /// </summary>
     public partial class App : Application
     {
-        internal void InitializeComponent()
+        public IServiceProvider ServiceProvider { get; private set; }
+
+        public IConfiguration Configuration { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
         {
-            // Nothing
+            var builder = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            Configuration = builder.Build();
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+
+            var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient(typeof(MainWindow));
+
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            services.AddSingleton<IFontProvider, FontProvider>();
+            services.AddSingleton<IImageProvider, ImageProvider>();
+
+            services.AddSingleton<IPainter, Painter>();
+
+            services.AddSingleton<ICardBuilder, CardBuilder>();
         }
     }
 }
